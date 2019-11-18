77d4ef65f4860398a175c7b8fef5faed7e18e72b
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2019-11-18 11:27:58 +0200

    updated

--- insertti.php
+++ insertti.php
@@ -2,9 +2,17 @@
 
 include "db.php";
 
-$sql = "INSERT INTO jotain (, , )
-VALUES ('jotain', 'jotain', 'jotain')";
+$sql = "INSERT INTO users (id, email, pwd)
+VALUES ('$email', '$pwd',)";
 
+if ($conn->query($sql) === TRUE) 
 {
     header('Location: location.php');
+}
+else 
+{
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+
 }
