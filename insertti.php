6904cda11289dc13b9bbe55778d4b6b8feb12b41
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2019-11-26 12:40:43 +0200

    pwd check

--- insertti.php
+++ insertti.php
@@ -2,17 +2,29 @@
 
 include "db.php";
 
-$sql = "INSERT INTO users (id, email, pwd)
-VALUES ('$email', '$pwd',)";
+// Tarkista tuletko lomakkeelta
 
-if ($conn->query($sql) === TRUE) 
-{
-    header('Location: location.php');
-}
-else 
-{
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-$conn->close();
+// Tarkista onko tiedot oikein
+
+// salasanan hash password_hash() -funktiolla
+if (filter_has_var(INPUT_POST, 'submit')){
+     
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd']; 
+
+    $hashed_pwd = password_hash($pwd, PASSWORD_DEFAULT); 
+
+    $sql = "INSERT INTO users (email, pwd) VALUES ('$email','$hashed_pwd')";
+
+    if ($conn->query($sql) === TRUE) 
+    {
+        header('Location: location.php');
+    }
+    else 
+    {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
 
 }
