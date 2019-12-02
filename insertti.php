9b197707e3fee9c0195f121ed07baba15d25ecf4
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2019-12-02 09:45:45 +0200

    execute

--- insertti.php
+++ insertti.php
@@ -6,12 +6,12 @@ include "db.php";
 
 // Tarkista onko tiedot oikein
 
-// salasanan hash password_hash() -funktiolla
+
 if (filter_has_var(INPUT_POST, 'submit')){
 
-    // email tsekkaus
+    
 
-    // salasanan tsekkaus
+    
 
     $email = $_POST['email'];
     $pwd = $_POST['pwd']; 
@@ -19,7 +19,7 @@ if (filter_has_var(INPUT_POST, 'submit')){
     $hashed_pwd = password_hash($pwd, PASSWORD_DEFAULT); 
 
 
-    // Prepared statement
+    
 
 $stmt = $conn->prepare("INSERT INTO users (email, pwd) VALUES (?, ?)");
 $stmt->bind_param("ss", $email, $hashed_pwd);
