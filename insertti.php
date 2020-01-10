7fc106dd1d038fb213518c1b7416172114d3375a
Markus <36534781+markuspylkkanen@users.noreply.github.com>
2020-01-10 09:22:23 +0200

    Add files via upload

---
+++ insertti.php
@@ -0,0 +1,42 @@
+<?php
+
+include "db.php";
+
+// Tarkista tuletko lomakkeelta
+
+// Tarkista onko tiedot oikein
+
+
+if (filter_has_var(INPUT_POST, 'submit')){
+
+    
+
+    
+    
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd']; 
+    
+    if (strlen($pwd) < 8) {
+        $msg = 'Salasanan pitää olla vähintään 8 merkkiä pitkä';
+        header('Location: register.php?msg='.$msg);
+        die();
+    }
+
+    $hashed_pwd = password_hash($pwd, PASSWORD_DEFAULT); 
+
+    $stmt = $conn->prepare("INSERT INTO users (email, pwd) VALUES (?, ?)");
+    $stmt->bind_param("ss", $email, $hashed_pwd);
+
+    if ($stmt->execute() === TRUE) 
+    {
+        header('Location: location.php');
+    }
+    else 
+    {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+
+}
