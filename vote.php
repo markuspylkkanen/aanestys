8a12c6c69222767b9ebe83ccf9788a779a923b4f
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2020-02-10 10:53:20 +0200

    method post

--- vote.php
+++ vote.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset)
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         include "db.php";
 
@@ -13,33 +13,47 @@ if (isset)
         $stmt = $conn->prepare("INSERT INTO vote (aihe) VALUES (?)");
         $stmt->bind_param("s");
 
-        if ($stmt->execute() === TRUE) 
+        // if ($stmt->execute() === TRUE) 
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title> Luo äänestys </title> 
+</head>
+<body>
 
-<title> Luo äänestys </title> 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 <h4> Voit tällä sivulla luoda äänestyksen! </h4>
-<br>
-    <div class="form-group">
-        <label for="kysymys">Äänestyksen aihe:</label>
-        <input type="text" name="kysymys">
+
+<form action="vote.php" method="post">
+
+        <div class="form-group">
+                <label for="kysymys">Äänestyksen aihe:</label>
+                <input type="text" name="kysymys">
+        </div>
         
-<button class="btn-primary" onclick="addChoice()">Lisää vaihtoehtoja</button>
-</div>
-<br>
+        
     
-<div class="form-group">
-        <input type="text" name="vaihtoehto" placeholder="Vaihtoehto 1">
+        <div class="form-group">
+                <input type="text" name="vaihtoehto" placeholder="Vaihtoehto 1">
         
-        <a href="" class="button">Tallenna</a>
-      </div>
+                <a href="#" onclick="addChoice()">Lisää vaihtoehtoja</a>
+                
+        </div>
 
-    <div id="choices">
+        <div id="choices">
 
-    </div>
+        </div>
+        
+        <button class="btn btn-primary" class="button">Tallenna</button>
+        
 
+</form>
   
     
 
@@ -72,7 +86,8 @@ let newDiv = document.createElement('div');
 }
 </script>
 
-
+</body>
+</html>
 
 
 
