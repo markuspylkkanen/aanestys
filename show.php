5100da8a3c00577e93873f82b2c34577a1295c1e
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2020-02-12 13:03:51 +0200

    wasd

--- show.php
+++ show.php
@@ -1,1 +1,22 @@
 <?php
+
+if (isset($_GET['id'])) {
+    echo "juup";
+}
+
+
+include "db.php";
+$sql = "SELECT * FROM vote";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["id"]. " - kysymys: ". $row["aihe"];
+    }
+} else {
+    echo "ei ole kysymyksiä";
+}
+
+$conn->close();
+?>
