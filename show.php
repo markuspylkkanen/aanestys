586a40db65db37341933340bfa73dc8e0f35189a
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2020-02-13 12:30:48 +0200

    vaihtoehdot

--- show.php
+++ show.php
@@ -20,4 +20,26 @@ if ($result->num_rows > 0) {
 }
 
 $conn->close();
+
+
+if (!isset($_GET['id'])) {
+    die('dead');
+}
+$last_id = intval($_GET['id']);
+
+
+include "db.php";
+$sql = "SELECT * FROM optio WHERE id = $last_id";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["id"]. " - Vaihtoehdot: ". $row["optio_value"];
+    }
+} else {
+    echo "ei ole vaihtoehtoja";
+}
+
+$conn->close();
 ?>
