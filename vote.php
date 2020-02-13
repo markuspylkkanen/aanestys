98096b971edeeda983589424002a53395076309e
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2020-02-13 11:11:47 +0200

    lastid pois

--- vote.php
+++ vote.php
@@ -24,7 +24,7 @@ $aihe = $_POST['kysymys'];
                                 $stmt->execute();
                         }        
                 }
-                $last_id = 
+                
                 header('Location: show.php?id='.$last_id);
 
         } else {
