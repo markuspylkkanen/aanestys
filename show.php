441e9edb11829f6402091e8a8dcfcdf90bd68a0a
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2020-02-14 12:26:07 +0200

    a href

--- show.php
+++ show.php
@@ -1,4 +1,4 @@
-<button type="button" class="btn btn-danger">Poista äänestyksiä</button>
+<a href="del.php"  class="btn btn-danger">Poista äänestyksiä</button>
 <br>
 <?php
 
@@ -45,7 +45,6 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 
-"DELETE FROM vote WHERE   ";
 
 ?>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
