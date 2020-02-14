d77fb7bc5cc8250889baa167c6023d4d3368f427
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2020-02-14 10:58:23 +0200

    boostrap

--- show.php
+++ show.php
@@ -1,3 +1,5 @@
+<button type="button" class="btn btn-danger">Poista äänestyksiä</button>
+<button type="button" class="btn btn-primary">Muokkaa</button><br>
 <?php
 
 if (!isset($_GET['id'])) {
@@ -42,4 +44,6 @@ if ($result->num_rows > 0) {
 }
 
 $conn->close();
+
 ?>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
