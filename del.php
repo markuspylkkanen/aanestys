91b2b1170aea98716d2bfcf20690861f7736e082
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2020-03-06 11:44:52 +0200

    asgdtadafdra

--- del.php
+++ del.php
@@ -1,7 +1,20 @@
 <?php
+// Poistaa sekä vaihtoehdon, että äänestyksen id:n perusteella
 
+// Tarkista onko GET-parametri annettu:
+if (!isset($_GET['id'])) {
+    die('dead');
+}
+
+// ID
+$id = intval($_GET['id']);
+
+
+// kohde taulu: vote tai option?
+$table = $_GET['vote'];
+
+// yhdistetään tietokantaan
 include "db.php";
 
-$id = $_GET['id']
-
-"DELETE FROM vote WHERE   ";
+// Luodaan ja suoritetaan sql-lause annettujen parametrien mukaan
+"DELETE FROM $table WHERE id = ? ";
