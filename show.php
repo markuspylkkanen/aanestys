1af5c787e6ebc86e8a7f83f860bee926e1e46cf0
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2020-03-13 10:57:31 +0200

    merienda fonts

--- show.php
+++ show.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>votet</title>
+    <link rel="stylesheet" type="text/css" href="on.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Merienda&display=swap" rel="stylesheet">
 </head>
 <body>
-
 <h2>Poista äänestyksiä tai vaihtoehtoja</h2>
 <br>
 
