0fae9db492661813f15983f3275c15b239c19e1c
Markus <36534781+markuspylkkanen@users.noreply.github.com>
2020-06-16 09:29:14 +0300

    Add files via upload

---
+++ register.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+	<title>Rekisteröinti</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
+<body>
+<h5> Rekisteröidy! <h5>
+
+<?php if (isset($_GET['msg'])):?>
+  <p class="alert alert-danger"><?php echo $_GET['msg']; ?></p>
+<?php endif; ?>
+
+<br>
+<form class="form-inline" action="insertti.php" method="post">
+  <label for="email">Sähköposti:</label>
+  <input type="email" class="form-control" id="email" name="email">
+  
+  <label for="pwd">Salasana:</label>
+  <input type="password" class="form-control" id="pwd" name="pwd">
+  
+  <div class="form-check"></div>
+
+  <button type="submit" name="submit" class="btn btn-primary">Luo tili</button>
+
+</form>
+</body>
+</html>
